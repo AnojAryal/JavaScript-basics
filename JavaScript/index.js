732911fb9ea453f654ab7cc631934438ca26cecc
FraNzY <anoj1810@gmail.com>
2023-06-12 00:02:59 +0545

    learning JavaScript from scratch

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -46,3 +46,96 @@ function square(number){
     return number * number;
 }
 console.log(square(2));
+
+//operators
+
+//Arthmetic operators 
+
+let x= 15
+let y = 5
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+//increment ++
+console.log(x++);
+console.log(x);
+
+//decrement --
+console.log(--x);
+
+
+//Assignmant operators
+
+x = x+5 ;
+x+= 5 ;
+
+x = x*5 ;
+x*= 5;
+
+//in above expression both the cases are
+// same we can assign both at once too
+
+//comparison operators
+ console.log(x> 10);
+ console.log(x>=20);
+ console.log(x<10);
+ console.log(x<= 10);
+
+ console.log(x ===10);
+ console.log(x !== 10);
+
+
+ //equality operators
+
+ //strict equality (type and values)
+ console.log(1 ===1);
+ console.log('1' ===1);
+
+ //lose equality --only focuses on value
+ console.log(1 == 1);
+ console.log(true == 1);
+ console.log('1' == 1);
+
+ //ternary operators 
+
+ let points = 120
+
+ let type = points > 100 ? 'gold customer' : 'silver customer'
+ console.log(type);
+
+ //logical operators
+
+ //AND &&
+ console.log(true && true);
+ console.log(false && true);
+
+ let highIncome = true;
+ let goodCreditScore = false;
+ let eligibleForLoan = highIncome && goodCreditScore
+ console.log(eligibleForLoan ? 'yes loan granted': 'loan not granted');
+
+ //OR ||
+ eligibleForLoan = highIncome || goodCreditScore
+ console.log(eligibleForLoan ? 'yes loan granted': 'loan not granted')
+
+ //truthy and falsy 
+ let userColor = 'red';
+ let defaultColor = 'green'
+ let currentColor = userColor || defaultColor
+ console.log(currentColor);
+
+ //Not !
+ console.log(!eligibleForLoan)
+
+//bitwise operator
+ console.log(1 | 2); //bitwise OR
+ console.log(1 & 2); //bitwise AND 
+
+ // to read 00000100
+ // to red / write 00000110
+ //read write execute 00000111
