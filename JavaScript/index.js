b3a4d2d2925a978445cc2599f1b8ab7bbfef5e87
anojaryal <anoj1810@gmail.com>
2023-09-12 21:14:57 +0545

    Revising JavaScript from scratch

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -801,3 +801,38 @@ console.log(`The day of the week for ${month1}/${day1}/${year1} is ${dayOfWeek}.
 const fir = 12
 const sec = 18
 console.log("the sum of 2 numbers is :",fir+sec);
+
+
+// program to check if a number is prime or not
+
+// take input from the user
+const numberp = parseInt(prompt("Enter a positive number: "));
+let isPrime = true;
+
+// check if number is equal to 1
+if (numberp === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+
+// check if number is greater than 1
+else if (numberp > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < numberp; i++) {
+        if (numberp % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${numberp} is a prime number`);
+    } else {
+        console.log(`${numberp} is a not prime number`);
+    }
+}
+
+// check if number is less than 1
+else {
+    console.log("The number is not a prime number.");
+}
